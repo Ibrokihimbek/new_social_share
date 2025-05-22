2a56820366a830401bed3c04f3b05756f42f800e
krishna <krishnamusalimadugu@gmail.com>
2025-05-22 11:11:57 +0530

    fix : commented registrar plugin because it is deprecated and causing trouble for new flutter versions

--- android/src/main/kotlin/com/ibrohim/new_social_share/new_social_share/SocialSharePlugin.kt
+++ android/src/main/kotlin/com/ibrohim/new_social_share/new_social_share/SocialSharePlugin.kt
@@ -16,6 +16,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+// commented this line so that flutter plugin can support for 3.29 version
 //import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.net.URLEncoder
